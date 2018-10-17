850f2afc6f8789ea68a42b8d92318ed7be3ce07e
btmccollum <btmcco8@gmail.com>
2018-10-17 16:12:53 -0500

    adjusted signup process, updated minor layouts and css

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,12 +7,35 @@ class UserController < ApplicationController
         erb :'/users/login'
     end
 
-    post '/signup' do
+    post '/users/signup' do
+        binding.pry
+        if !params.any?{|key, value| value == ""}
+            @user = User.new(:username => params["username"], :email => params["email"], :password => params["password"], :reputation => 0)
+            if @user.save
+                session[:user_id] = @user.id
+                flash[:message] = "Account was successfully created."
+                
+                redirect '/'
+            else
+                flash[:message] = "Uh oh! Something went wrong. Please try again." 
+                redirect '/users/signup'
+            end
+        else
+            flash[:message] = "Invalid information provided. All fields are required. Please try again." 
+            redirect '/users/signup'
+        end
     end
 
     post '/login' do 
     end
 
-    get '/logout' do
+    get '/users/logout' do
+        if logged_in
+            session.clear
+            redirect '/'
+        else 
+            flash[:message] = "You cannot log out if you weren't ever logged in!" 
+            redirect '/'
+        end
     end
 end
