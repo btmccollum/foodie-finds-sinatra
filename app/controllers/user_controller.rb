8b6cf039e066d5871e36f47d8cd08d171bb108f7
btmccollum <btmcco8@gmail.com>
2018-10-17 14:28:36 -0500

    created basic user controller template, added base user views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,16 @@
 class UserController < ApplicationController
+    get '/signup' do
+    end
+
+    get '/login' do
+    end
+
+    post '/signup' do
+    end
+
+    post '/login' do 
+    end
+
+    get '/logout' do
+    end
 end
