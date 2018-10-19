59f9322d5c98f2b4212d38c25e7f3932f15366c2
btmccollum <btmcco8@gmail.com>
2018-10-19 14:47:24 -0500

    added comments new route, added comments new view

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,2 +1,13 @@
 class CommentController < ApplicationController
+    get '/categories/:category/posts/:id/comments/new' do
+        @category = Category.find_by(title: params[:category])
+        @post = Post.find(params[:id])
+
+        if logged_in
+            erb :'/comments/new'
+        else 
+            flash[:message] = "You must be logged in to reply." 
+            redirect "/categories/#{@category.title}/posts/#{@post.id}"
+        end
+    end
 end
