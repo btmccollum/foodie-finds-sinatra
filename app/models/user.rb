12c2d08d9ff798983e941af1875022af60119a6c
btmccollum <btmcco8@gmail.com>
2018-10-22 16:31:50 -0500

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
     has_many :comments
     belongs_to :category
 
+    validates_presence_of :username, :email, :password_digest
+    validates_uniqueness_of :username, :email
+    
     has_secure_password
 end
